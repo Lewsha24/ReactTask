09fd91dbbc6a7e11eedc3f40592c0a1626dd2c95
Lewsha24 <sadkovoi0@gmail.com>
2024-05-20 15:44:27 +0700

    Nice Answer

--- src/App.js
+++ src/App.js
@@ -3,31 +3,56 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import {useFetch} from "./useFetch";
 
+//Task 2
+//Answer 2
 function App() {
-        const {
-            data,
-            isLoading,
-            error,
-            refetch
-        } = useFetch('https://jsonplaceholder.typicode.com/posts');
-    console.log(isLoading)
+    import { useLocalStorage } from './useLocalStorage';
+    const [token, { setItem, removeItem }] = useLocalStorage('token');
+
     return (
         <div>
+            <p>
+                Твой токен: { token }
+            </p>
             <div>
-                <button onClick={() => refetch({
-                    params: {
-                        _limit: 3
-                    }
-                })}>
-                    Перезапросить
+                <button onClick={() => setItem('new-token')}>
+                    Задать токен
+                </button>
+                <button onClick={() => removeItem()}>
+                    Удалить токен
                 </button>
             </div>
-            {isLoading && 'Загрузка...'}
-            {error && 'Произошла ошибка'}
-            {data && !isLoading && data.map(item => <div key={item.id}>{item.title}</div>) }
         </div>
     );
 }
+
+// Task 1
+// Answer 1
+// function App() {
+//         const {
+//             data,
+//             isLoading,
+//             error,
+//             refetch
+//         } = useFetch('https://jsonplaceholder.typicode.com/posts');
+//     console.log(isLoading)
+//     return (
+//         <div>
+//             <div>
+//                 <button onClick={() => refetch({
+//                     params: {
+//                         _limit: 3
+//                     }
+//                 })}>
+//                     Перезапросить
+//                 </button>
+//             </div>
+//             {isLoading && 'Загрузка...'}
+//             {error && 'Произошла ошибка'}
+//             {data && !isLoading && data.map(item => <div key={item.id}>{item.title}</div>) }
+//         </div>
+//     );
+// }
 
 
 export default App;
